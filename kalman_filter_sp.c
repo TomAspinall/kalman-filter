2259a4d2fdc1939d4aaecc72cc7e48f38a83afa3
TomAspinall <tomaspinall2512@gmail.com>
2024-11-01 20:14:23 +1000

    working example

--- kalman_filter_sp.c
+++ kalman_filter_sp.c
@@ -1,10 +1,44 @@
 #include <Python.h>
+#include "numpy/arrayobject.h"
 
 // Logical function:
-int ckalman_filter(int n, int m)
+int ckalman_filter_sequential(
+    // n: the total number of observations
+    int n,
+    // m: the dimension of the state vector
+    int m,
+    // d: the dimension of observations
+    int d,
+    // a0:
+    double *a0)
 {
-    printf("Debug: calling ckalman_filter(%d)\n", n);
-    printf("Debug: calling ckalman_filter(%d)\n", m);
+
+    printf("Debug: n is: (%d)\n", n);
+    printf("Debug: m is: (%d)\n", m);
+    printf("Debug: d is: (%d)\n", d);
+    printf("Debug: *a0 is: (%d)\n", *a0);
+
+    // Array dimensions:
+    int m_x_m = m * m;
+    int m_x_d = m * d;
+    int na_sum;
+    int t = 0;
+
+    // integers and double precisions used in dcopy and dgemm
+    int intone = 1;
+    double dblone = 1.0, dblminusone = -1.0, dblzero = 0.0;
+    // To transpose or not transpose matrix
+    char *transpose = "T", *dont_transpose = "N";
+
+    // SEQUENTIAL PROCESSING DEFINED VARIABLES:
+    int N_obs = 0;
+
+    // Doubles for the Sequential Processing iteration:
+    double V;
+    double Ft;
+    double tmpFtinv;
+
+    // PyArray_malloc();
     if (n < m)
         return n;
     else
@@ -14,17 +48,24 @@ int ckalman_filter(int n, int m)
 /* Wrapped ckalman_filter function */
 static PyObject *kalman_filter(PyObject *self, PyObject *args)
 {
-    int n, m;
+    PyObject *a0;
+    int n, m, d;
 
     /* Parse the input, from Python integer to C int */
-    if (!PyArg_ParseTuple(args, "ii", &n, &m))
+    if (!PyArg_ParseTuple(args, "iiO", &m, &d, &a0))
         return NULL;
+    n = PyObject_Length(a0);
+    // Exception handling:
+    if (n < 0)
+    {
+        return NULL;
+    }
     /* If the above function returns -1, an appropriate Python exception will
      * have been set, and the function simply returns NULL
      */
 
     /* Construct the result: a Python integer object */
-    return Py_BuildValue("i", ckalman_filter(n, m));
+    return Py_BuildValue("ii", ckalman_filter_sequential(n, m, d, &a0), d);
 }
 
 /* Define functions in module */
@@ -48,5 +89,13 @@ static struct PyModuleDef KalmanFilterStruct = {
 /* Module initialization */
 PyObject *PyInit_kalman_filter(void)
 {
+
+    import_array(); // Inititalise Numpy
+    if (PyErr_Occurred())
+    {
+        printf("Failed to import numpy Python module(s).");
+        return NULL;
+    }
+
     return PyModule_Create(&KalmanFilterStruct);
 }
--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import Extension, setup
 
-module = Extension("kalman_filter", sources=["kalman_filter_sp.c"])
+import numpy as np
+
+module = Extension("kalman_filter", sources=[
+                   "kalman_filter_sp.c"])
 
 setup(
     name="example",
     version="0.0.1",
     description="Efficient Kalman Filtering and Smoothing through the sequential processing method",
-    ext_modules=[module])
+    ext_modules=[module],
+    include_dirs=[np.get_include()]
+)
 
 # python setup.py build
