2a618cfa930a5b0d1bb452315156b44d86efb233
TomAspinall <tomaspinall2512@gmail.com>
2025-04-17 20:16:31 +1000

    dedicated exceptions.py file

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+
+# Attempting to Smooth before Kalman filter executed:
+class KalmanFilterNotExecuted(Exception):
+    pass
+
+# A provided input to a dataclass is considered out of range:
+
+
+class InputOutOfRange(Exception):
+    pass
