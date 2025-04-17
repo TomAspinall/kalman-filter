f748416b16123b469ff03daf3c66d116f6c5bc6e
TomAspinall <tomaspinall2512@gmail.com>
2025-04-17 21:13:10 +1000

    moving directories

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import numpy as np
 cblas_include_dir = "C:\\Users\\Tpm Aspinall\\Documents\\Work\\OpenBLAS-0.3.28-x64-64\\include\\"
 cblas_library_dir = "C:\\Users\\Tpm Aspinall\\Documents\\Work\\OpenBLAS-0.3.28-x64-64\\lib\\"
 numpy_include_dir = np.get_include()
-include_dirs = [numpy_include_dir, cblas_include_dir, '.']
+include_dirs = [numpy_include_dir, cblas_include_dir, 'src/']
 library_dirs = [cblas_library_dir]
 
 c_sources = ["kalman_filter_verbose.c", "utils.c",
@@ -18,9 +18,9 @@ module = Extension("kalman_filter", sources=c_sources,
                    library_dirs=[cblas_library_dir])
 
 setup(
-    name="example",
+    name="kalman_filter",
     version="0.0.1",
-    description="Efficient Kalman Filtering and Smoothing through the sequential processing method",
+    description="Efficient Traditional Kalman Filtering and Smoothing using the sequential processing algorithm",
     ext_modules=[module],
     include_dirs=include_dirs
 )
