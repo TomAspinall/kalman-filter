6f1ed27bfc556aac27faf1266ddc73a3faf46c17
TomAspinall <tomaspinall2512@gmail.com>
2025-04-18 16:09:20 +1000

    split kalman_filter verbose argument into a separate function to maintain type hinting of function outputs. correct methods for smoothing applied

--- kalman_filter/__init__.py
+++ kalman_filter/__init__.py
@@ -1,7 +1,8 @@
 
-from .methods import (kalman_filter, kalman_filter_optimise, kalman_smoother,
+from .methods import (kalman_filter, kalman_filter_optimise,
+                      kalman_filter_verbose, kalman_smoother,
                       kalman_smoother_optimise)
 from .models import KalmanFilter, KalmanFiltered, KalmanSmoothed
 
 __all__ = ["KalmanFilter", "KalmanFiltered", "KalmanSmoothed", "kalman_filter",
-           "kalman_filter_optimise", "kalman_smoother", "kalman_smoother_optimise"]
+           "kalman_filter_optimise", "kalman_filter_verbose", "kalman_smoother", "kalman_smoother_optimise"]
--- kalman_filter/methods.py
+++ kalman_filter/methods.py
@@ -4,17 +4,11 @@ from .models import KalmanFilter, KalmanFiltered, KalmanSmoothed
 from .src import kalman_filter as KF
 
 
-def kalman_filter(filter: KalmanFilter, verbose: bool = False) -> float:
+def kalman_filter(filter: KalmanFilter) -> float:
     """Perform an optimized Kalman filter to calculate the maximum likelihoood estimate of current inputs against observations"""
     filter_input = filter.to_dict()
-    if not verbose:
-        # Call compiled C-Code with validated inputs:
-        return KF.kalman_filter(filter_input)
-    else:
-        # Call compiled C-Code with validated inputs:
-        output = KF.kalman_filter_verbose(filter_input)
-        # Return validated outputs:
-        return KalmanFiltered.from_dict(output | filter_input)
+    # Call compiled C-Code with validated inputs:
+    return KF.kalman_filter(filter_input)
 
 
 def kalman_filter_optimise(filter: dict[str, ndarray]) -> float:
@@ -27,19 +21,37 @@ def kalman_filter_optimise(filter: dict[str, ndarray]) -> float:
     return KF.kalman_filter(filter)
 
 
-def kalman_smoother(filter: KalmanFilter | KalmanFiltered) -> KalmanSmoothed:
+def kalman_filter_verbose(filter: KalmanFilter) -> KalmanFiltered:
+    """Perform an optimized Kalman filter to calculate the maximum likelihoood estimate of current inputs against observations"""
     filter_input = filter.to_dict()
+    # Call compiled C-Code with validated inputs:
+    output = KF.kalman_filter_verbose(filter_input)
+    # Return validated outputs:
+    return KalmanFiltered.from_dict(output | filter_input)
+
+
+def kalman_smoother(smoother: KalmanFilter | KalmanFiltered) -> KalmanSmoothed:
     # Instance requires filtering, and then smoothing, values:
-    if isinstance(filter, KalmanFilter):
-        output = KF.kalman_filter_smoother(filter_input)
+    if isinstance(smoother, KalmanFilter):
+        filter_input = smoother.to_dict()
+        filter_output = KF.kalman_filter_verbose(filter_input)
+        smoother_input = filter_input | filter_output
     # Instance has been filtered, only smoothing required:
-    elif isinstance(filter, KalmanFiltered):
-        output = KF.kalman_smoother(filter_input)
+    elif isinstance(smoother, KalmanFiltered):
+        smoother_input = filter_input | smoother.to_dict()
+    else:
+        raise TypeError(
+            "arg: `filter` is not type `KalmanFilter` or `KalmanFiltered`!")
+
+    # Call compiled C-Code with validated inputs:
+    output = KF.kalman_smoother(smoother_input)
     return KalmanSmoothed.from_dict(output | filter_input)
 
 
-def kalman_smoother_optimise(filter: dict[str, ndarray], filtered: bool = False):
+def kalman_smoother_optimise(smoother: dict[str, ndarray], filtered: bool = False) -> dict[str, float | ndarray]:
     if filtered:
-        return KF.kalman_smoother(filter)
+        smoother_input = smoother
     else:
-        return KF.kalman_filter_smoother(filter)
+        filter_output = KF.kalman_filter_verbose(smoother)
+        smoother_input = smoother | filter_output
+    return KF.kalman_smoother(smoother_input)
