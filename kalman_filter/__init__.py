328cf3f77672731777c403c0be0c8c5e308b400f
TomAspinall <tomaspinall2512@gmail.com>
2025-05-05 20:29:24 +1000

    iterate version

--- kalman_filter/__init__.py
+++ kalman_filter/__init__.py
@@ -2,7 +2,7 @@
 from .methods import kalman_filter, kalman_filter_verbose, kalman_smoother
 from .models import KalmanFilter, KalmanFiltered, KalmanSmoothed
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 __all__ = ["KalmanFilter", "KalmanFiltered", "KalmanSmoothed",
            "kalman_filter", "kalman_filter_verbose", "kalman_smoother"]
