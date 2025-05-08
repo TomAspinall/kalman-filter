59ad3876e251a97b8ac34d442e5bca2376892609
TomAspinall <tomaspinall2512@gmail.com>
2025-05-08 20:13:48 +1000

    update .gitignore, pyproject.toml, __init__.py, exceptions.py

--- kalman_filter/__init__.py
+++ kalman_filter/__init__.py
@@ -1,8 +1,10 @@
 
+from typing import Final, LiteralString
+
 from .methods import kalman_filter, kalman_filter_verbose, kalman_smoother
 from .models import KalmanFilter, KalmanFiltered, KalmanSmoothed
 
-__version__ = "1.0.0"
+__version__: Final[LiteralString] = "1.0.0"
 
 __all__ = ["KalmanFilter", "KalmanFiltered", "KalmanSmoothed",
            "kalman_filter", "kalman_filter_verbose", "kalman_smoother"]
