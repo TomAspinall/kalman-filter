c5f77394f128b70f6c709e97f7ba28cbbdb6ab08
TomAspinall <tomaspinall2512@gmail.com>
2025-05-21 22:40:15 +1000

    `test_treering` now succeeds given prior hotfix to NAN memory allocation

--- test/test_treering.py
+++ test/test_treering.py
@@ -4,17 +4,5 @@ test = KalmanFilterBaseTest("test/data/treering")
 
 if __name__ == "__main__":
     test.run_test()
-    test.test_ndim()
-    test.test_log_likelihood()
-    test.test_filtered_outputs()
-    test.test_smoothed_outputs()
-
-    expected = test.expected_filtered.to_dict()
-    import kalman_filter as kf
-    actual = kf.kalman_filter_verbose(test.test_KalmanFilter())
-
-    # Output arrays need to be initially populated with na's, as they're curently random memory pointers.
-    actual['vt']
-    expected['vt']
 
     print("Treering test success!")
